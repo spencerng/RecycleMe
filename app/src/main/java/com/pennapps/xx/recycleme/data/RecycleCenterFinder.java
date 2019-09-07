5aa7ae4d97791105ad62341ffd90892584621fbf
Spencer Ng <sng1488@gmail.com>
2019-09-06 22:28:37 -0400

    Modify recycle center finder method arguments

--- app/src/main/java/com/pennapps/xx/recycleme/data/RecycleCenterFinder.java
+++ app/src/main/java/com/pennapps/xx/recycleme/data/RecycleCenterFinder.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class RecycleCenterFinder {
 
-    public static ArrayList<RecycleCenter> getRecycleCenters(String objectType) {
+    public static ArrayList<RecycleCenter> getRecycleCenters(String objectType, String zipCode) {
         return new ArrayList<>();
     }
 
